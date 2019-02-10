afca57af7089de3151170c421932c26a0de60d13
rafaelabne123 <44871767+rafaelabne123@users.noreply.github.com>
2019-02-10 18:10:27 -0200

    atualizado

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 const app = new Vue({
     el: '#app',
     data: {
-        message: 'Olá Mundo',
-        list: [
-            {text: 'item 1'},
-            {text: 'item 2'},
-            {text: 'item 3'},
-            {text: 'item 4'}
+        pokemonList: [
+            {name: 'Charizard', number: '006'},
+            {name: 'Squirtle', number: '007'},
+            {name: 'Pikachu', number: '025'},
+            {name: 'Celebi', number: '251'},
+            {name: 'Lucario', number: '448'},
+            {name: 'Delphox', number: '655'},
         ]
-        
     }
 })
